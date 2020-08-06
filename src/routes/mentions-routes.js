a462403f21c736109f41eef9ed368b1ac1258686
adaao <armascarelli@gmail.com>
2020-08-06 08:52:42 -0300

    Update mention

--- src/routes/mentions-routes.js
+++ src/routes/mentions-routes.js
@@ -4,9 +4,26 @@ const { check } = require('express-validator');
 const mentiosnController = require('../controllers/mentions-controller');
 
 router.get('/', mentiosnController.listMentions);
+
+function validateFriendName(friendName){
+   return check('friend').isLength({ min: 7 }).withMessage("O nome precisa ter no mínimo 7 caracteres");
+}
+
+function validateMention(mention){
+   return check('mention').isLength({ min: 20, max: 280}).withMessage("A menção precisa ter no mínimo 20 e no máximo 280 caracteres.");
+}
+
 router.post('/', [
-   check('friend').isLength({ min: 7 }).withMessage("O nome precisa ter no mínimo 7 caracteres"),
-   check('mention').isLength({ min: 20}).withMessage("A menção precisa ter no mínimo 20 e no máximo 280 caracteres.")
+   validateFriendName('friend'),
+   validateMention('mention')
 ], mentiosnController.createMention);
 
+router.put('/:id', [
+   check('friend').optional().isLength({ min: 7 }).withMessage("O nome precisa ter no mínimo 7 caracteres"),
+   check('mention').optional().isLength({ min: 20}).withMessage("A menção precisa ter no mínimo 20 e no máximo 280 caracteres.")
+], mentiosnController.updateMention);
+
+router.delete('/:id', (req, res) => { console.log('defina a função deletar aqui')});
+
 module.exports = router;
+
